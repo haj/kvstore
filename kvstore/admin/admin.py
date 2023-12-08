73e29b8cd034560f8b4a75a3d3148fb8f58934e3
Hassan Ajan <hassanajan@Hassans-MacBook-Pro.local>
2023-12-08 18:59:37 +0100

    update to django 4

--- kvstore/admin/admin.py
+++ kvstore/admin/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 
 from kvstore.admin.views import upload, upload_bulk, upload_csv
@@ -11,18 +11,18 @@ class KVStoreAdminApp(admin.ModelAdmin):
 
     def get_urls(self):
         return [
-            url(
-                r'^kvstore/upload/?$',
+            path(
+                'kvstore/upload/',
                 self.admin_site.admin_view(upload),
                 name="kvstore_upload_ui"
             ),
-            url(
-                r'^kvstore/upload_bulk/?$',
+            path(
+                'kvstore/upload_bulk/',
                 self.admin_site.admin_view(upload_bulk),
                 name="kvstore_upload_bulk"
             ),
-            url(
-                r'^kvstore/upload_csv/?$',
+            path(
+                'kvstore/upload_csv/',
                 self.admin_site.admin_view(upload_csv),
                 name="kvstore_upload_csv"
             ),
